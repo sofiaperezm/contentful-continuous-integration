9eb9bba4b857556a44cf39eaa29b271b0f16ae7f
sofiaperezm <sofi.prz.m@gmail.com>
2024-12-27 13:09:16 -0500

    feat: add migration scripts, migration logic and circleci config file

---
+++ migration/2.js
@@ -0,0 +1,4 @@
+module.exports = function runMigration(migration) {
+    const post = migration.editContentType('post');
+    post.createField('author').name('author').type('Symbol').required(false);
+};
